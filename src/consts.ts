32d4213c5466c5842bcf9c3bea2d269696e1b7dc
Gabriel Lacorte <gabriellacorte@posteo.com>
2025-03-26 17:19:35 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,13 +3,13 @@ import type { Metadata, Site, Socials, Skill } from "@types";
 export const SITE: Site = {
     TITLE: "Gabriel L",
     DESCRIPTION:
-        "My personal website where I write about topics about software development and cybersecurity.",
+        "Personal website where I write about software development and cybersecurity.",
     EMAIL: "gabriellacorte@posteo.com",
 };
 
 export const HOME: Metadata = {
     TITLE: "Home",
-    DESCRIPTION: "A backend developer and cybersecurity enthusiast.",
+    DESCRIPTION: "A backend developer",
 };
 
 export const BLOG: Metadata = {
@@ -18,12 +18,10 @@ export const BLOG: Metadata = {
 };
 
 export const SKILLS: Skill[] = [
-    "Backend Development (Node.js (Express, NextJS), Python (Flask, FastAPI), Go (Gin))",
-    "Frontend Development (React, Astro, Tailwind CSS)",
+    "Backend Development (Python (Flask, FastAPI), Go (Gin), Node.js)",
+    "Frontend Development (React, NextJS, Astro)",
     "DevOps (Docker, Linux, Git, CI/CD)",
-    "Database Management (SQL, Prisma, SQLAlchemy)",
     "Cyber Security (Offensive, Active Directory)",
-    "Low-level Programming (C, Assembly (x86_64))",
 ];
 
 export const SOCIALS: Socials = [
@@ -33,6 +31,6 @@ export const SOCIALS: Socials = [
     },
     {
         NAME: "LinkedIn",
-        HREF: "https://www.linkedin.com/in/gabriel-lacorte/",
+        HREF: "https://www.linkedin.com/in/gabriel-lacorte-dev/",
     },
 ];
